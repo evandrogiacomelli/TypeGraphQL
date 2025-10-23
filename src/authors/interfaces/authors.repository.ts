159779c7ae06eee11836f3a689394c5ff88edb58
EvandroGiacomelli <evandrogiacomelli3@gmail.com>
2025-10-23 15:16:33 -0300

    authors resolvers, interfaces, repository

---
+++ src/authors/interfaces/authors.repository.ts
@@ -0,0 +1,29 @@
+import { ICreateAuthor } from '@/authors/interfaces/create-author'
+import { Author } from '@/authors/graphql/models/author'
+
+export type SearchParams = {
+  page?: number;
+  perPage?: number;
+  filter?: string;
+  sort?: string;
+  sortDir?: 'asc' | 'desc';
+}
+
+export type SearchResult = {
+  items: Author[];
+  currentPage: number;
+  perPage: number;
+  lastPage: number;
+  total: number;
+}
+
+export interface IAuthorsRepository {
+  sortableFields: String[];
+  create(data: ICreateAuthor) : Promise<Author>;
+  update(author: Author) : Promise<Author>;
+  delete(id: String) : Promise<Author>;
+  findById(id: string) : Promise<Author>;
+  findByEmail(email: string) : Promise<Author>;
+  search(params: SearchParams) : Promise<SearchResult>;
+  get(id: string) : Promise<Author>;
+}
