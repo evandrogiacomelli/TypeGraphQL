7722f9353076f3267682ec9c079d8adc67e059e5
EvandroGiacomelli <evandrogiacomelli3@gmail.com>
2025-10-27 15:28:53 -0300

    Should only apply pagination when the parameters are null, leftover comennted code vfor debugging

--- src/authors/repositories/authors-prisma.repository.int-spec.ts
+++ src/authors/repositories/authors-prisma.repository.int-spec.ts
@@ -1,10 +1,11 @@
 import { beforeEach, describe } from 'node:test'
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthorsPrismaRepository } from '@/authors/repositories/authors-prisma.repository'
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 import { execSync } from 'node:child_process'
 import { NotFoundError } from '@/shared/errors/not-found-error'
 import { AuthorDataBuilder } from '@/authors/helpers/author-data-builder'
+import { Author } from '@/authors/graphql/models/author'
 
 describe( 'AuthorsPrismaRepository', () => {
   let module: TestingModule;
@@ -52,5 +53,59 @@ describe( 'AuthorsPrismaRepository', () => {
 
     const res = await repository.findById(author.id);
     expect(author).toMatchObject(res);
+
+    await prisma.author.deleteMany();
   })
+
+
+})
+
+describe('search method', () => {
+  const prisma = new PrismaClient();
+  const repository = new AuthorsPrismaRepository(prisma as any);
+
+  beforeEach(async () => {
+    await prisma.author.deleteMany();
+  })
+
+  test('Should only apply pagination when the parameters are null', async () => {
+    const createdAt = new Date();
+    const data: Prisma.AuthorCreateManyInput[] = [];
+
+    Array.from({ length: 16 }).forEach((_, index) => {
+      const timeStamp = createdAt.getTime() + index;
+
+      // chama o builder dentro do loop para gerar dados únicos
+      const author = AuthorDataBuilder({});
+
+      data.push({
+        ...author,
+        email: `author${index}@a.com`, // garante unicidade
+        createdAt: new Date(timeStamp),
+      });
+    });
+
+    // const data : Prisma.AuthorCreateManyInput[] = [];
+    // for (let i = 0; i < 16; i++) {
+    //   data.push({
+    //     name: `test${i}`,
+    //     email: `test${i}@gtest.com`,
+    //     createdAt: new Date(),
+    //   })
+    // }
+
+    await prisma.author.createMany({ data });
+
+    // const allAuthors = await prisma.author.findMany();
+    // console.log('todos os autores no banco:', allAuthors);
+
+    const result = await repository.search({});
+
+    expect(result.total).toBe(16);
+    expect(result.items.length).toBe(15);
+    result.items.forEach((item) => {
+      expect(item.id).toBeDefined();
+    })
+  });
+
 })
